ddba9eb1737f877d5d250e80507a30f21f9d7e4a
Martin Bianchi <tinchola7@gmail.com>
2020-11-13 10:17:36 -0300

    feat: load fonts with font face on init

---
+++ src/loadFonts.js
@@ -0,0 +1,27 @@
+export const loadFonts = () => {
+    const fonts = document.createElement('style');
+    fonts.innerHTML = `
+    @font-face {
+      font-family: "Basis Grotesque Pro";
+      src: local("Basis Grotesque Pro"), local("Basis Grotesque Pro"),
+        url(https://assets.every.org/every-month/BasisGrotesque-Regular-Pro.woff2) format("woff2");
+      font-weight: normal;
+      font-style: normal;
+    }
+    @font-face {
+      font-family: "Basis Grotesque Pro";
+      src: local("Basis Grotesque Pro"), local("Basis Grotesque Pro"),
+        url(https://assets.every.org/every-month/BasisGrotesque-Bold-Pro.woff2) format("woff2");
+      font-weight: 700;
+      font-style: normal;
+    }
+    @font-face {
+      font-family: "Basis Grotesque Pro";
+      src: local("Basis Grotesque Pro"), local("Basis Grotesque Pro"),
+        url(https://assets.every.org/every-month/BasisGrotesque-Medium-Pro.woff2) format("woff2");
+      font-weight: 500;
+      font-style: normal;
+    }
+    `
+    document.head.appendChild(fonts)
+}
