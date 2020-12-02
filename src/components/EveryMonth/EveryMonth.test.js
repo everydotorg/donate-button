04a51a87064d4e32e5570fc264b8265ce6a851ff
Omar Diab <me@omardiab.com>
2020-12-02 19:07:36 +0900

    update test for ava

--- src/components/EveryMonth/EveryMonth.test.js
+++ src/components/EveryMonth/EveryMonth.test.js
@@ -1,9 +1,11 @@
-;
-import { render } from '@testing-library/react';
+import { h } from 'preact';
+import test from 'ava';
+import { render } from '@testing-library/preact';
+
 import EveryMonth from './EveryMonth';
 
-test('renders', () => {
+test('renders', t => {
   const { getByText } = render(<EveryMonth />);
   const linkElement = getByText(/Every Month/i);
-  expect(linkElement).toBeInTheDocument();
+  t.truthy(linkElement);
 });
