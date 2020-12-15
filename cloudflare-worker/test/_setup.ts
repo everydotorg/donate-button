44c976a4d97275a4bf6ea0e2c5cf663b0347e4f6
Omar Diab <me@omardiab.com>
2020-12-15 18:09:12 +0900

    update comment about service worker mock

--- cloudflare-worker/test/_setup.ts
+++ cloudflare-worker/test/_setup.ts
@@ -1,4 +1,6 @@
-// set up global namespace for worker environment
+// setup mocks for the service worker environment
+// TODO: find an alternative to this since it seems like it's no longer being
+// maintained
 import makeServiceWorkerEnv = require("service-worker-mock");
 declare const global: WorkerGlobalScope;
 Object.assign(global, makeServiceWorkerEnv());
