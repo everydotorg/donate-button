69cfa9d5fd1a85c881c593e56914e30bb27e390e
Omar Diab <me@omardiab.com>
2020-12-16 17:46:48 +0900

    enable resolution by paths in webpack

--- cloudflare-worker/webpack.config.js
+++ cloudflare-worker/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
-const webpack = require("webpack");
-
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 const mode = process.env.NODE_ENV || "production";
 
 module.exports = {
@@ -11,7 +10,7 @@ module.exports = {
   mode,
   resolve: {
     extensions: [".ts", ".tsx", ".js"],
-    plugins: [],
+    plugins: [new TsconfigPathsPlugin({ configFile: "./tsconfig.json" })],
   },
   module: {
     rules: [
