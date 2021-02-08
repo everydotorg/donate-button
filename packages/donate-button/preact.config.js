6b13975002414fbf7ea9fd7270a387aa5b893212
Omar Diab <me@omardiab.com>
2021-02-08 17:26:37 +0900

    fix and lint preact config

--- packages/donate-button/preact.config.js
+++ packages/donate-button/preact.config.js
@@ -1,58 +1,61 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
-const semver = require('semver')
-const packageJson = require('./package.json')
+const path = require('path');
 
-const OUTPUT_FOLDER = path.join(__dirname, "dist")
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const semver = require('semver');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+const packageJson = require('./package.json');
+
+const OUTPUT_FOLDER = path.join(__dirname, 'dist');
 const VERSION = semver.parse(packageJson.version);
-const VERSION_SLUG = VERSION.major === 0
-  ? `${VERSION.major}.${VERSION.minor}`
-  : VERSION.major
-const VERSION_PATH = path.join("donate-button", VERSION_SLUG)
+const VERSION_SLUG =
+	VERSION.major === 0 ? `${VERSION.major}.${VERSION.minor}` : VERSION.major;
+const VERSION_PATH = path.join('donate-button', VERSION_SLUG);
 
 export default {
-  /**
-   * Function that mutates the original webpack config.
-   * Supports asynchronous changes when a promise is returned (or it's an async function).
-   *
-   * @param {object} config - original webpack config.
-   * @param {object} env - options passed to the CLI.
-   * @param {WebpackConfigHelpers} helpers - object with useful helpers for working with the webpack config.
-   * @param {object} options - this is mainly relevant for plugins (will always be empty in the config), default to an empty object
-   **/
-  webpack(config, env, helpers, options) {
-    delete config.entry.polyfills
-    config.output.path = path.resolve(OUTPUT_FOLDER, VERSION_PATH)
-    config.output.filename = "index.js"
+	/**
+	 * Function that mutates the original webpack config.
+	 * Supports asynchronous changes when a promise is returned (or it's an async function).
+	 *
+	 * @param {object} config - original webpack config.
+	 * @param {object} env - options passed to the CLI.
+	 * @param {WebpackConfigHelpers} helpers - object with useful helpers for working with the webpack config.
+	 * @param {object} _options - this is mainly relevant for plugins (will always be empty in the config), default to an empty object
+	 **/
+	webpack(config, env, helpers, _options) {
+		config.resolve.plugins = [new TsconfigPathsPlugin()];
+		delete config.entry.polyfills;
+		config.output.path = path.resolve(OUTPUT_FOLDER, VERSION_PATH);
+		config.output.filename = 'index.js';
 
-    if (env.production) {
-      const vercelBaseUrl = process.env.VERCEL_URL
-      config.output.publicPath = vercelBaseUrl
-        ? `https://${vercelBaseUrl}/`
-        : `https://assets.every.org/${VERSION_PATH}/`
-      console.log('Building for', config.output.publicPath)
+		if (env.production) {
+			const vercelBaseUrl = process.env.VERCEL_URL;
+			config.output.publicPath = vercelBaseUrl
+				? `https://${vercelBaseUrl}/`
+				: `https://assets.every.org/${VERSION_PATH}/`;
+			console.log('Building for', config.output.publicPath);
 
-      // Copy assets
-      config.plugins.push(
-        // need trailing slash to make sure copy plugin treats as directory
-        new CopyPlugin([{ from: 'public', to: `${config.output.path}/` }])
-      )
-    }
+			// Copy assets
+			config.plugins.push(
+				// Need trailing slash to make sure copy plugin treats as directory
+				new CopyPlugin([{from: 'public', to: `${config.output.path}/`}])
+			);
+		}
 
-    // Generate index.html directly instead of use a template
-    const plugHtml = helpers.getPluginsByName(config, 'HtmlWebpackPlugin')[0]
-    config.plugins[plugHtml.index] = new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: 'index.html',
-      inject: 'head',
-    })
+		// Generate index.html directly instead of use a template
+		const plugHtml = helpers.getPluginsByName(config, 'HtmlWebpackPlugin')[0];
+		config.plugins[plugHtml.index] = new HtmlWebpackPlugin({
+			template: './public/index.html',
+			filename: 'index.html',
+			inject: 'head'
+		});
 
-    // Remove css modules and add to string loader.
-    let { ruleIndex } = helpers.getLoadersByName(config, 'css-loader')[0]
-    config.module.rules[ruleIndex] = {
-      test: /\.css$/,
-      use: [{ loader: 'to-string-loader' }, { loader: 'css-loader' }],
-    }
-  },
-}
+		// Remove css modules and add to string loader.
+		const {ruleIndex} = helpers.getLoadersByName(config, 'css-loader')[0];
+		config.module.rules[ruleIndex] = {
+			test: /\.css$/,
+			use: [{loader: 'to-string-loader'}, {loader: 'css-loader'}]
+		};
+	}
+};
