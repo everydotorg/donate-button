95a9121eb6fd65bafea8969f0efa7e811bde000f
Omar Diab <me@omardiab.com>
2021-02-09 15:53:37 +0900

    simpler useI18n

--- packages/donate-button/src/hooks/use-i18n.ts
+++ packages/donate-button/src/hooks/use-i18n.ts
@@ -1,7 +1,6 @@
 import {useContext} from 'preact/hooks';
 import {OptionsContext} from 'src/contexts/options-context';
-
-const defaultI18n = 'en';
+import {I18NOptions} from 'src/helpers/options-types';
 
 const getBrowserLanguage = () => {
 	const language = window.navigator.language;
@@ -15,16 +14,13 @@ const getBrowserLanguage = () => {
 	return language;
 };
 
-const useI18n = () => {
+const useI18n = (): I18NOptions => {
 	const browserLanguage = getBrowserLanguage();
 	const {i18n, language: configuredLanguage} = useContext(OptionsContext);
 
-	// Language given in config has priority over user browser lang
-	if (i18n[configuredLanguage]) {
-		return i18n[configuredLanguage];
-	}
-
-	return i18n[browserLanguage] ? i18n[browserLanguage] : i18n[defaultI18n];
+	// Language given in config has priority over user browser lang; default to en
+	// since that's always provided by default options
+	return i18n[configuredLanguage] ?? i18n[browserLanguage] ?? i18n.en!;
 };
 
 export default useI18n;
