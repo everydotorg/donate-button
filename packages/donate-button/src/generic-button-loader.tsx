0adc36603fd2b485dc660264da9f757263aca45a
Martin Bianchi <tinchola7@gmail.com>
2021-03-25 13:38:45 -0300

    fix lint errors

--- packages/donate-button/src/generic-button-loader.tsx
+++ packages/donate-button/src/generic-button-loader.tsx
@@ -1,26 +1,54 @@
-import { render } from 'preact'
-import { GenericButtonProps, DonateButtonOptions } from "src/helpers/options-types"
+import {render} from 'preact';
+import {
+	GenericButtonProps,
+	DonateButtonOptions
+} from 'src/helpers/options-types';
 
 interface GenericButtonLoader {
-  selector: string;
-  onClick: Function;
-  widgetOptions: Partial<DonateButtonOptions>;
-  options: Partial<GenericButtonProps>;
+	selector: string;
+	onClick: () => void;
+	widgetOptions: Partial<DonateButtonOptions>;
+	options: Partial<GenericButtonProps>;
 }
 
-const genericButtonLoader = ({selector, options = {}, onClick, widgetOptions}: GenericButtonLoader) => {
-  const div = document.querySelector(selector);
-  
-	const charity = typeof widgetOptions?.onSubmit !== 'function' ? widgetOptions.onSubmit?.charity : ''
+const getCharityName = (
+	widgetOptions: Partial<DonateButtonOptions>
+): string => {
+	const genericFoundation = 'your-foundation';
+	if (typeof widgetOptions.onSubmit === 'function') {
+		return genericFoundation;
+	}
+
+	return widgetOptions.onSubmit?.charity
+		? widgetOptions.onSubmit?.charity
+		: genericFoundation;
+};
+
+const genericButtonLoader = ({
+	selector,
+	options = {},
+	onClick,
+	widgetOptions
+}: GenericButtonLoader) => {
+	const div = document.querySelector(selector);
+
+	const charity = getCharityName(widgetOptions);
 	const hrefUrl = `https://www.every.org/${charity}/donate`;
 
-  if(div){
-    import('./components/GenericButton').then(C => {
-      const genericButton = <C.default {...options} onClick={onClick} hrefUrl={hrefUrl}/>
-      render(genericButton, div)
-    })
-  }
-  
-}
+	if (div) {
+		import('./components/GenericButton')
+			.then((C) => {
+				const Button = C.default;
 
-export default genericButtonLoader
+				const GenericButton = (
+					<Button {...options} hrefUrl={hrefUrl} onClick={onClick} />
+				);
+				render(GenericButton, div);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	}
+};
+
+export default genericButtonLoader;
