ddc95e8833f4b59ecbfd8290e569d4e7b5e6554f
Omar Diab <me@omardiab.com>
2021-04-16 17:52:07 +0900

    fix storybook lint

---
+++ packages/donate-button/src/stories/generic-button.stories.tsx
@@ -0,0 +1,45 @@
+import {Story} from '@storybook/preact';
+import {ComponentProps} from 'preact';
+import GenericButton from 'src/components/GenericButton';
+
+const storybookSpec = {
+	title: 'Generic Button',
+	component: GenericButton
+};
+export default storybookSpec;
+
+const Template: Story<ComponentProps<typeof GenericButton>> = (args) => (
+	<GenericButton {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {};
+
+export const CustomLabel = Template.bind({});
+CustomLabel.args = {
+	label: 'Custom label'
+};
+
+export const WithoutLogo = Template.bind({});
+WithoutLogo.args = {
+	withLogo: false
+};
+
+export const Background = Template.bind({});
+Background.args = {
+	bgColor: 'black',
+	textColor: 'white'
+};
+
+export const FullConfiguration = Template.bind({});
+FullConfiguration.args = {
+	bgColor: '',
+	borderRadius: '',
+	classes: '',
+	fontSize: '',
+	hrefUrl: '',
+	label: 'Donate',
+	padding: '',
+	textColor: '',
+	withLogo: true
+};
