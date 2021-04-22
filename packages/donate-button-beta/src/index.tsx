546bb95cd3c07a7ce75df64de734146fbca8978c
Omar Diab <me@omardiab.com>
2021-04-22 15:41:32 +0900

    a bit more logs

--- packages/donate-button-beta/src/index.tsx
+++ packages/donate-button-beta/src/index.tsx
@@ -31,39 +31,51 @@ export default createButtonInSelector;
 loadFonts();
 
 /**
- * Extracts donate button options from an Every.org donate URL
- *
- * Expected format: http?://(www.)?every.org/:slug/donate(/crypto)?
- * @returns extracted options, or null if url isn't a valid Every.org donate URL
- */
-function optionsFromEdoUrl(url: string): DonateButtonOptions | null {
-	try {
-		const parsedUrl = new URL(url);
-		if (!['www.every.org', 'every.org'].includes(parsedUrl.host)) {
-			return null;
-		}
-
-		const pathParts = parsedUrl.pathname.split('/').filter(Boolean);
-		if (pathParts.length < 2 || pathParts[1] !== 'donate') {
-			return null;
-		}
-
-		const nonprofitSlug = pathParts[0];
-		const crypto = pathParts[2] === 'crypto';
-		return {nonprofitSlug, crypto};
-	} catch {
-		return null;
-	}
-}
-
-let initializingButtons = false;
-
-/**
  * Helper function to debug donate button issues
  */
 function log(...messages: unknown[]): void {
 	console.info('Every.org Donate Button:', ...messages);
 }
+
+enum OptionsParseErrorType {
+	BAD_DOMAIN = 'BAD_DOMAIN',
+	NOT_DONATE_ROUTE = 'NOT_DONATE_ROUTE',
+	OTHER = 'OTHER'
+}
+interface OptionsParseError {
+	errorType: OptionsParseErrorType;
+	error?: unknown;
+}
+
+/**
+ * Extracts donate button options from an Every.org donate URL
+ *
+ * Expected format: http?://(www.)?every.org/:slug/donate(/crypto)?
+ * @returns extracted options, or null if url isn't a valid Every.org donate URL
+ */
+function optionsFromEdoUrl(
+	url: string
+): DonateButtonOptions | OptionsParseError {
+	try {
+		const parsedUrl = new URL(url);
+		if (!['www.every.org', 'every.org'].includes(parsedUrl.host)) {
+			return {errorType: OptionsParseErrorType.BAD_DOMAIN};
+		}
+
+		const pathParts = parsedUrl.pathname.split('/').filter(Boolean);
+		if (pathParts.length < 2 || pathParts[1] !== 'donate') {
+			return {errorType: OptionsParseErrorType.NOT_DONATE_ROUTE};
+		}
+
+		const nonprofitSlug = pathParts[0];
+		const crypto = pathParts[2] === 'crypto';
+		return {nonprofitSlug, crypto};
+	} catch (error: unknown) {
+		return {errorType: OptionsParseErrorType.OTHER, error};
+	}
+}
+
+let initializingButtons = false;
 
 const DONATE_BUTTON_CLASS = 'edo-donate-btn';
 const INITIALIZED_ATTRIBUTE = 'data-edo-init';
@@ -93,8 +105,11 @@ function initButtons() {
 			}
 
 			const options = optionsFromEdoUrl(href);
-			if (!options) {
-				log('could not extract options from link', buttonLink);
+			if ('errorType' in options) {
+				log('could not extract options from link', {
+					buttonLink,
+					errorData: options
+				});
 				continue;
 			}
 
