284e888b267351266f3a1f1bb0ea36606d8a1416
Martin Bianchi <tinchola7@gmail.com>
2021-05-11 15:46:53 -0300

    feat: mobile-desktop layout. nonprofit information

--- packages/donate-button-beta/src/components/widget/index.tsx
+++ packages/donate-button-beta/src/components/widget/index.tsx
@@ -1,5 +1,8 @@
 import cxs from 'cxs';
 import {Frequency} from 'src/components/widget/Frequency';
+import { NonprofitHeader } from 'src/components/widget/NonprofitHeader';
+import { NonprofitInfo } from 'src/components/widget/NonprofitInfo';
+import { BREAKPOINTS } from 'src/components/widget/theme/breakpoints.enum';
 import {COLORS} from 'src/components/widget/theme/colors.enum';
 
 cxs.prefix('edoWidget-');
@@ -7,7 +10,7 @@ cxs.prefix('edoWidget-');
 const wrapperCss = cxs({
 	position: 'absolute',
 	height: '100vh',
-	width: '100vh',
+	width: '100vw',
 	zIndex: 999,
 	top: 0,
 	bottom: 0,
@@ -29,24 +32,79 @@ const widgetCss = cxs({
 
 	display: 'grid',
 	gridTemplateColumns: '60% 40%',
+	gridTemplateRows: '1fr max-content max-content',
 	background: 'white',
-	borderRadius: '24px'
+	borderRadius: '24px',
+	overflow: 'hidden',
+
+	[`${BREAKPOINTS.Tablet}`]:{
+		gridTemplateRows: 'max-content 1fr max-content',
+		width: '100vw',
+		height: '100vh',
+		borderRadius: 'unset'
+	}
 });
 
 const formCss = cxs({
 	gridColumn: '1 / 2',
+	gridRow: '1 / 3',
 	borderRight: `1px solid ${COLORS.LightGray}`,
-	padding: '1.5rem'
+	padding: '1.5rem',
+
+	[`${BREAKPOINTS.Tablet}`]: {
+		borderRight: 'none'
+	}
 });
+
+const nonProfitHeaderCss = cxs({
+	gridColumn: '2 / 3',
+	gridRow: '1 / 2',
+
+	[`${BREAKPOINTS.Tablet}`]: {
+		height: '190px',
+		gridColumn: '1 / -1',
+		gridRow: '1 / 2'
+	}
+})
+
+const nonProfitInfoCss = cxs({
+	gridColumn: '2 / 3',
+	gridRow: '2 / 4',
+})
+
+const ctaCss = cxs({
+	gridColumn: '1 / 2',
+	gridRow: '3 / 4',
+	[`${BREAKPOINTS.Tablet}`]: {
+		gridColumn: '1 / -1',
+		gridRow: '3 / 4'
+	}
+})
+
+const scrollableContent = cxs({
+	display:'contents',
+
+	[`${BREAKPOINTS.Tablet}`]: {
+		display: 'flex',
+		flexDirection: 'column',
+		overflow: 'auto',
+		gridColumn: '1 / -1',
+		gridRow: '2 / 3'
+	}
+})
 
 const Widget = ({show}: {show: boolean}) => {
 	return show ? (
 		<div className={wrapperCss}>
 			<form className={widgetCss}>
-				<div className={formCss}>
-					<Frequency />
+				<div className={scrollableContent}>
+					<div className={formCss}>
+						<Frequency />
+					</div>
+					<NonprofitInfo classes={[nonProfitInfoCss]} />
 				</div>
-				<div>WIP</div>
+				<button className={ctaCss}>Donate</button>
+				<NonprofitHeader classes={[nonProfitHeaderCss]} />
 			</form>
 		</div>
 	) : null;
---
+++ packages/donate-button-beta/src/components/widget/theme/breakpoints.enum.ts
@@ -0,0 +1,5 @@
+export enum BREAKPOINTS {
+    Desktop = '@media only screen and (max-width:64em)',
+    Tablet = '@media only screen and (max-width:48em)',
+    Mobile = '@media only screen and (max-width:27em)'
+}
