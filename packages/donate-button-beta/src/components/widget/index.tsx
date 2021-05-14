6ca64f836c01ecf21b921f37d7712c0fca573f1e
Martin Bianchi <tinchola7@gmail.com>
2021-05-14 10:25:49 -0300

    feat: modify breakpoint. make mobile-first

--- packages/donate-button-beta/src/components/widget/index.tsx
+++ packages/donate-button-beta/src/components/widget/index.tsx
@@ -26,44 +26,47 @@ const wrapperCss = cxs({
 });
 
 const widgetCss = cxs({
-	// Temporary until we have more content inside the widget
-	height: '70vh',
-	width: '60vw',
 
-	display: 'grid',
-	gridTemplateColumns: '60% 40%',
-	gridTemplateRows: '1fr max-content max-content',
+
 	background: 'white',
-	borderRadius: '24px',
 	overflow: 'hidden',
+	
+	display: 'grid',
+	gridTemplateRows: 'max-content 1fr max-content',
+	width: '100vw',
+	height: '100vh',
+	borderRadius: 'unset',
 
-	[`${BREAKPOINTS.Tablet}`]: {
-		gridTemplateRows: 'max-content 1fr max-content',
-		width: '100vw',
-		height: '100vh',
-		borderRadius: 'unset'
+	[`${BREAKPOINTS.TabletLandscapeUp}`]: {
+		// Temporary until we have more content inside the widget
+		gridTemplateColumns: '60% 40%',
+		gridTemplateRows: '1fr max-content max-content',
+		height: '70vh',
+		width: '60vw',
+		borderRadius: '24px',
 	}
 });
 
 const formCss = cxs({
 	gridColumn: '1 / 2',
 	gridRow: '1 / 3',
-	borderRight: `1px solid ${COLORS.LightGray}`,
 	padding: '1.5rem',
+	borderRight: 'none',
 
-	[`${BREAKPOINTS.Tablet}`]: {
-		borderRight: 'none'
+	[`${BREAKPOINTS.TabletLandscapeUp}`]: {
+		borderRight: `1px solid ${COLORS.LightGray}`,
 	}
 });
 
 const nonProfitHeaderCss = cxs({
-	gridColumn: '2 / 3',
+	height: '190px',
+	gridColumn: '1 / -1',
 	gridRow: '1 / 2',
 
-	[`${BREAKPOINTS.Tablet}`]: {
-		height: '190px',
-		gridColumn: '1 / -1',
-		gridRow: '1 / 2'
+	[`${BREAKPOINTS.TabletLandscapeUp}`]: {
+		height: 'auto',
+		gridColumn: '2 / 3',
+		gridRow: '1 / 2',
 	}
 });
 
@@ -73,23 +76,25 @@ const nonProfitInfoCss = cxs({
 });
 
 const ctaCss = cxs({
-	gridColumn: '1 / 2',
+	gridColumn: '1 / -1',
 	gridRow: '3 / 4',
-	[`${BREAKPOINTS.Tablet}`]: {
-		gridColumn: '1 / -1',
-		gridRow: '3 / 4'
+
+	[`${BREAKPOINTS.TabletLandscapeUp}`]: {
+		gridColumn: '1 / 2',
+		gridRow: '3 / 4',
 	}
 });
 
 const scrollableContent = cxs({
-	display: 'contents',
+	display: 'flex',
+	flexDirection: 'column',
+	overflow: 'auto',
+	gridColumn: '1 / -1',
+	gridRow: '2 / 3',
 
-	[`${BREAKPOINTS.Tablet}`]: {
-		display: 'flex',
-		flexDirection: 'column',
-		overflow: 'auto',
-		gridColumn: '1 / -1',
-		gridRow: '2 / 3'
+	[`${BREAKPOINTS.TabletLandscapeUp}`]: {
+		display: 'contents',
+		overflow: 'initial'
 	}
 });
 
