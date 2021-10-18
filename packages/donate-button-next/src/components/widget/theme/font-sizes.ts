de3421127692e7635c06ced69b04be9cf2ad84ac
Julian <poma.nob@gmail.com>
2021-10-18 12:44:36 -0300

    fix: lilbub overrides

--- packages/donate-button-next/src/components/widget/theme/font-sizes.ts
+++ packages/donate-button-next/src/components/widget/theme/font-sizes.ts
@@ -3,30 +3,35 @@ import cxs from 'cxs';
 export const smallText: cxs.CSSObject = {
 	fontSize: '12px',
 	lineHeight: '16px',
-	letterSpacing: '0.01em'
+	letterSpacing: '0.01em',
+	fontWeight: 400
 };
 
 export const bodyText: cxs.CSSObject = {
 	fontSize: '16px',
 	lineHeight: '24px',
-	letterSpacing: '-0.01em'
+	letterSpacing: '-0.01em',
+	fontWeight: 400
 };
 
 export const inputText: cxs.CSSObject = {
 	fontSize: '16px',
-	lineHeight: '16px'
+	lineHeight: '16px',
+	fontWeight: 400
 };
 
 export const headingText: cxs.CSSObject = {
 	fontSize: '24px',
 	lineHeight: '28px',
-	letterSpacing: '-0.015em'
+	letterSpacing: '-0.015em',
+	fontWeight: 700
 };
 
 export const heading2Text: cxs.CSSObject = {
 	fontSize: '24px',
 	lineHeight: '28px',
-	letterSpacing: '-0.015em'
+	letterSpacing: '-0.015em',
+	fontWeight: 700
 };
 
 export const heading3Text: cxs.CSSObject = {
