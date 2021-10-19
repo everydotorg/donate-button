fc0671561e7391fd61938c2a786489c58ac104b7
Julian <poma.nob@gmail.com>
2021-10-19 11:31:44 -0300

    feat: v0.3 with new styles & shadow dom

--- packages/donate-button-beta/src/components/widget/types/nonprofit-info.ts
+++
@@ -1,9 +0,0 @@
-import {DonationRecipient} from 'src/components/widget/types/donation-recipient';
-
-export interface NonprofitInfo {
-	name: string;
-	logo: string;
-	backgroundImage: string;
-	description: string;
-	countries: DonationRecipient[];
-}
--- packages/donate-button-beta/src/helpers/string-is-url.ts
+++
@@ -1,9 +0,0 @@
-export const isUrl = (text: string): boolean => {
-	try {
-		const url = new URL(text);
-
-		return ['http:', 'https:'].includes(url.protocol);
-	} catch {
-		return false;
-	}
-};
