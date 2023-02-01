82a6d9014e0cc5be31ab7fb3c91fe9457fa4f1a0
Fishbakh-N <nikita.fishbakh.work@gmail.com>
2023-02-01 09:05:55 +0200

    remove fundraiser card

--- packages/donate-button-v4/src/components/widget/components/NonprofitInfo/index.tsx
+++ packages/donate-button-v4/src/components/widget/components/NonprofitInfo/index.tsx
@@ -1,13 +1,5 @@
-import {FundraiserCard} from 'src/components/widget/components/NonprofitInfo/FundraiserCard';
 import {NonprofitCard} from 'src/components/widget/components/NonprofitInfo/NonprofitCard';
-import {useConfigContext} from 'src/components/widget/hooks/useConfigContext';
 
 export const NonprofitInfo = () => {
-	const {fundraiserSlug} = useConfigContext();
-
-	if (fundraiserSlug) {
-		return <FundraiserCard />;
-	}
-
 	return <NonprofitCard />;
 };
