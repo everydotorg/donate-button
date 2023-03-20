1b191d2043bcf57bbd3ba26e165978d88fefd73c
Nikita Fishbakh <nikita.fishbakh.work@gmail.com>
2023-03-20 14:05:27 +0200

    Proper cloudinary flags

--- packages/donate-button-v4/src/helpers/getCloudinaryUrl.ts
+++ packages/donate-button-v4/src/helpers/getCloudinaryUrl.ts
@@ -1,5 +1,7 @@
 import {BASE_CLOUDINARY_URL} from 'src/constants/url';
 
+const FLAGS = 'f_auto,q_auto/';
+
 export const getCloudinaryUrl = (cloudinaryId: string) => {
-	return `${BASE_CLOUDINARY_URL}${cloudinaryId}`;
+	return `${BASE_CLOUDINARY_URL}${FLAGS}${cloudinaryId}`;
 };
