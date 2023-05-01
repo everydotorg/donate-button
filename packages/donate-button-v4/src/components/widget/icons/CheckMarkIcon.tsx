8ade3b7ba12c11044cf2f5b16c8bb25298d11f7a
Nikita Fishbakh <nikita.fishbakh.work@gmail.com>
2023-05-01 12:14:59 +0300

    create custom checkbox

---
+++ packages/donate-button-v4/src/components/widget/icons/CheckMarkIcon.tsx
@@ -0,0 +1,17 @@
+export const CheckMarkIcon = ({className}: {className?: string}) => (
+	<svg
+		fill="none"
+		width="24"
+		height="24"
+		viewBox="0 0 24 24"
+		xmlns="http://www.w3.org/2000/svg"
+		className={className}
+	>
+		<path
+			d="M20 6L9 17l-5-5"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			strokeWidth="2"
+		/>
+	</svg>
+);
