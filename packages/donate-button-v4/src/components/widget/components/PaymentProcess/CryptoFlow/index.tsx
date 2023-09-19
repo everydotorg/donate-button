c7792efff96a6b22344e11fb1a1c057952b1c20a
Nikita Fishbakh <nikita.fishbakh.work@gmail.com>
2023-09-19 10:49:37 +0300

    respect nonprofit.metadata.disablePrivateNotes

--- packages/donate-button-v4/src/components/widget/components/PaymentProcess/CryptoFlow/index.tsx
+++ packages/donate-button-v4/src/components/widget/components/PaymentProcess/CryptoFlow/index.tsx
@@ -14,6 +14,7 @@ import {
 	formCss,
 	legendCss
 } from 'src/components/widget/components/PaymentProcess/styles';
+import {useNonprofitOrError} from 'src/components/widget/hooks/useNonprofit';
 import {useSubmitDonation} from 'src/components/widget/hooks/useSubmitDonation';
 import {useWidgetContext} from 'src/components/widget/hooks/useWidgetContext';
 import {verticalStackCss, Spacing} from 'src/components/widget/theme/spacing';
@@ -23,6 +24,7 @@ import {getSubmitButtonText} from 'src/helpers/getSubmitButtonText';
 export const CryptoFlow = () => {
 	const submitDonation = useSubmitDonation();
 	const {cryptoAmount, cryptoCurrency} = useWidgetContext();
+	const {metadata} = useNonprofitOrError();
 
 	return (
 		<form className={formCss} onSubmit={submitDonation}>
@@ -43,7 +45,7 @@ export const CryptoFlow = () => {
 						</div>
 					)}
 				</fieldset>
-				<PrivateNote />
+				{!metadata?.disablePrivateNotes && <PrivateNote />}
 				<SubmitButton disabled={!cryptoAmount || !cryptoCurrency}>
 					{getSubmitButtonText({method: PaymentMethod.CRYPTO, cryptoCurrency})}
 				</SubmitButton>
