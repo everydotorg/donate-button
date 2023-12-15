8dba50023d93db99fbd5c537fa68192d72b9f0b6
Fishbakh-N <54896745+Fishbakh-N@users.noreply.github.com>
2023-12-15 21:18:30 +0200

    Fundraiser card (#380)
    * create fundraiser card
    
    * image size
    
    * add fundraiser example

--- packages/donate-button-v4/src/components/widget/components/NonprofitInfo/index.tsx
+++ packages/donate-button-v4/src/components/widget/components/NonprofitInfo/index.tsx
@@ -1,5 +1,13 @@
+import {FundraiserCard} from 'src/components/widget/components/NonprofitInfo/FundraiserCard';
 import {NonprofitCard} from 'src/components/widget/components/NonprofitInfo/NonprofitCard';
+import {useFundraiserOrUndefined} from 'src/components/widget/hooks/useFundraiser';
 
 export const NonprofitInfo = () => {
+	const fundraiser = useFundraiserOrUndefined();
+
+	if (fundraiser) {
+		return <FundraiserCard fundraiser={fundraiser} />;
+	}
+
 	return <NonprofitCard />;
 };
