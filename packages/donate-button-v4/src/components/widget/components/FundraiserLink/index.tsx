0b621c9ad60f546e5cf26de6294d895a018e0610
Fishbakh-N <nikita.fishbakh.work@gmail.com>
2024-01-18 10:24:10 +0200

    Support nonprofit.metadata.hideFundraiseButton

--- packages/donate-button-v4/src/components/widget/components/FundraiserLink/index.tsx
+++ packages/donate-button-v4/src/components/widget/components/FundraiserLink/index.tsx
@@ -4,10 +4,16 @@ import {
 } from 'src/components/widget/components/FundraiserLink/styles';
 import {GridCard} from 'src/components/widget/components/GridCard';
 import {useConfigContext} from 'src/components/widget/hooks/useConfigContext';
+import {useNonprofitOrError} from 'src/components/widget/hooks/useNonprofit';
 import {BASE_URL, FUNDRAISER_ROUTE} from 'src/constants/url';
 
 export const FundraiserLink = () => {
 	const {primaryColor, nonprofitSlug} = useConfigContext();
+	const nonprofit = useNonprofitOrError();
+
+	if (nonprofit.metadata?.hideFundraiseButton) {
+		return null;
+	}
 
 	const href = BASE_URL + nonprofitSlug + '/' + FUNDRAISER_ROUTE;
 
--- packages/donate-button-v4/src/components/widget/types/Nonprofit.ts
+++ packages/donate-button-v4/src/components/widget/types/Nonprofit.ts
@@ -19,6 +19,7 @@ export interface Nonprofit {
 	metadata?: {
 		disablePrivateNotes?: boolean;
 		prefixWithThe?: boolean;
+		hideFundraiseButton?: boolean;
 	};
 }
 
