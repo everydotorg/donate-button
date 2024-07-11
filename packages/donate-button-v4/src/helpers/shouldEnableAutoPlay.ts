451a5b7c69d388b25ae3341c8dca29be790e20f8
Fishbakh-N <nikita.fishbakh.work@gmail.com>
2024-07-11 10:49:36 +0300

    feat: Add getScriptParameters helper function

--- packages/donate-button-v4/src/helpers/shouldEnableAutoPlay.ts
+++ packages/donate-button-v4/src/helpers/shouldEnableAutoPlay.ts
@@ -1,20 +1,12 @@
+import getScriptParameters from 'src/helpers/getScriptParameters';
+
 const AUTO_PLAY_PARAM = 'explicit';
 
 export function shouldEnableAutoPlay() {
-	const parametersString = document.currentScript
-		?.getAttribute('src')
-		?.split('?')[1];
-	const parametersArray = parametersString
-		?.split('&')
-		.map((parameterString) => {
-			const [key, value] = parameterString.split('=');
-			return [key, value];
-		});
-	const parametersObject =
-		parametersArray && Object.fromEntries(parametersArray);
+	const parameters = getScriptParameters();
 
-	if (parametersObject) {
-		return parametersObject[AUTO_PLAY_PARAM] !== '1';
+	if (parameters) {
+		return parameters[AUTO_PLAY_PARAM] !== '1';
 	}
 
 	return true;
