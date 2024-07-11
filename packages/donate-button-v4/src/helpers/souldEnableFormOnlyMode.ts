581f17f85cd5d87d429c11fea395a23ddc9de622
Fishbakh-N <nikita.fishbakh.work@gmail.com>
2024-07-11 10:49:49 +0300

    feat: Add shouldEnableFormOnlyMode helper function

---
+++ packages/donate-button-v4/src/helpers/souldEnableFormOnlyMode.ts
@@ -0,0 +1,13 @@
+import getScriptParameters from 'src/helpers/getScriptParameters';
+
+const FORM_ONLY_PARAM = 'formOnly';
+
+export function shouldEnableFormOnlyMode() {
+	const parameters = getScriptParameters();
+
+	if (parameters) {
+		return parameters[FORM_ONLY_PARAM] !== '1';
+	}
+
+	return true;
+}
